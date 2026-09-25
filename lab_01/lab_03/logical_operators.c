49fff2ccde678d9eb33e89de4f92401650aeecb8
poonamgirwar <poonamgirwarr@gmail.com>
2026-09-25 15:51:46 +0530

    Create logical_operators.c

---
+++ lab_01/lab_03/logical_operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("poonam girwar\n enter first");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("result of '&&': %d\n", a&&b);
+    printf("result of '!': %d\n", !a);
+    printf("result of '||' : %d\n", a||b);
+    return 0;
+
+}
