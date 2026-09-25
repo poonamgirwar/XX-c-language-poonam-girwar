acc4855b10dd79517848e7799a5dfd3c22863818
poonamgirwar <poonamgirwarr@gmail.com>
2026-09-25 16:39:59 +0530

    Create bitwise_operators.c

---
+++ lab_03/bitwise_operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main()
+ {
+    int a = 12 ;
+    int b = 20 ;
+    printf("poonam girwar\n");
+    printf("a & b : %d\n", a & b);
+    printf("a | b : %d\n", a | b);
+    printf("a ^ b : %d\n", a ^ b);
+    printf("a << b : %d\n", a << b);
+    printf("a >> b : %d\n", a >> b);
+    return 0 ; 
+    }
