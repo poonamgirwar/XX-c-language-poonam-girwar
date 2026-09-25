4dcb957897b99d91f38b0cacacb80eab4360ca56
poonamgirwar <poonamgirwarr@gmail.com>
2026-09-25 17:07:12 +0530

    Create increment_operator.c

---
+++ lab_04/increment_operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10 ;
+    int b = ++a ;
+    int c = a++ ;
+    printf("poonam girwar\n");
+    printf("%d\n",a);
+    printf("%d\n",b);
+    printf("%d\n",c);
+    return 0 ;
+}
