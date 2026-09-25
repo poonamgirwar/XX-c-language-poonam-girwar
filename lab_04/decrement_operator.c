444df66d008991565c74ec7b56956707fae131ae
poonamgirwar <poonamgirwarr@gmail.com>
2026-09-25 17:12:50 +0530

    Create decrement_operator.c

---
+++ lab_04/decrement_operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a = 50 ;
+    int b = --a ;
+    int c = b-- ;
+    printf("poonam girwar\n");
+    printf("%d\n",a);
+    printf("%d\n",b);
+    printf("%d\n",c);
+    return 0 ;
+}
